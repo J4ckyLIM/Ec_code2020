0284a08d365a2dce7883568277e383fd3331f60c
JackyLIM <jacky.lim@edu.itescia.fr>
2020-06-24 23:54:25 +0200

    Feat: Remove search bar

--- view/favoriteMediaListView.php
+++ view/favoriteMediaListView.php
@@ -1,21 +1,5 @@
 <?php ob_start(); ?>
 
-<div class="row">
-    <div class="col-md-4 offset-md-8">
-        <form method="get">
-            <div class="form-group has-btn">
-                <input type="search" id="search" name="title" value="<?= $search; ?>" class="form-control"
-                       placeholder="Rechercher un film ou une série">
-
-                <button type="submit" class="btn btn-block bg-red">Valider</button>
-            </div>
-            <span class="error-msg">
-              <?= isset( $error_msg ) ? $error_msg : null; ?>
-            </span>
-        </form>
-    </div>
-</div>
-
 <h3>Mes favoris</h3>
 <div class="media-list" data="<?php $medias ?>">
     <?php foreach( $medias as $media ): ?>

bd27f5d378bf7a88116456530f17de64c4b96852
JackyLIM <jacky.lim@edu.itescia.fr>
2020-06-24 08:37:41 +0200

    Feat: Add values to media, alter media table, Add table type for media

--- codflix.sql
+++ codflix.sql
@@ -43,7 +43,28 @@ CREATE TABLE `genre` (
 INSERT INTO `genre` (`id`, `name`) VALUES
 (1, 'Action'),
 (2, 'Horreur'),
-(3, 'Science-Fiction');
+(3, 'Science-Fiction'),
+(4, 'Aventure');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `type`
+--
+
+DROP TABLE IF EXISTS `type`;
+CREATE TABLE `type` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `type`
+--
+
+INSERT INTO `type` (`id`, `name`) VALUES
+(1, 'Film'),
+(2, 'Série');
 
 -- --------------------------------------------------------
 
@@ -56,7 +77,7 @@ CREATE TABLE `media` (
   `id` int(11) NOT NULL,
   `genre_id` int(11) NOT NULL,
   `title` varchar(100) NOT NULL,
-  `type` varchar(20) NOT NULL,
+  `type_id` varchar(20) NOT NULL,
   `status` varchar(20) NOT NULL,
   `release_date` date NOT NULL,
   `summary` longtext NOT NULL,
@@ -64,6 +85,16 @@ CREATE TABLE `media` (
   `season` int(11) NULL,
   `trailer_url` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `media`
+--
+
+INSERT INTO `media` (`id`, `genre_id`,`title`,`type_id`,`status`,`release_date`,`summary`,`duration`, `season`, `trailer_url`) VALUES
+(1, 1, 'Le roi lion', 1, 'publié', '2019-08-07',"Le jour se lève sur la savane africaine. Les animaux de toutes sorte engagent un périple vers un piton rocheux. C'est le jour de la présentation aux animaux de Simba, fils du roi lion Mufasa et de la reine lionne Sarabi. Mufasa rend ensuite visite à son frère Scar, qui n'est pas venu à la cérémonie. Ce dernier est jaloux de son frère et convoite son trône. Avec les hyènes, il va échafauder un plan pour le débarrasser de son frère et de Simba, pour s'en emparer.", 126, 0, 'https://www.youtube.com/watch?v=tvvQitXftGk'),
+(2, 2, 'Saw', 1, 'publié', '2019-06-11', "Un lieutenant de police locale et son nouveau partenaire enquêtent sur une série de meurtres plus macabres les uns que les autres. Le mode opératoire rappelle étrangement celui d'un tueur en série ayant sévit autrefois dans la ville. Mais pris au piège sans le savoir, il va se retrouver en plein cœur d'un stratagème tordu et terrifiant dont seul le tueur tire les ficelles.", 185, 0, 'https://www.youtube.com/watch?v=lWmTi-VD6Ss'),
+(3, 1, 'Sonic', 1, 'publié', '2020-02-12', "L'histoire du hérisson bleu le plus rapide du monde qui arrive sur Terre, sa nouvelle maison. Sonic et son nouveau meilleur ami Tom font équipe pour sauver la planète du diabolique Dr. Robotnik, bien déterminé à régner sur le monde entier.", 99, 0 ,'https://www.youtube.com/watch?v=NCZTYdAP6w0'),
+(4, 3, 'Alita: Battle Angel', 1,'publié', '2020-02-13',"Lorsqu’Alita se réveille sans aucun souvenir de qui elle est, dans un futur qu’elle ne reconnaît pas, elle est accueillie par Ido, un médecin qui comprend que derrière ce corps de cyborg abandonné, se cache une jeune femme au passé extraordinaire. Ce n’est que lorsque les forces dangereuses et corrompues qui gèrent la ville d’Iron City se lancent à sa poursuite qu’Alita découvre la clé de son passé - elle a des capacités de combat uniques, que ceux qui détiennent le pouvoir veulent absolument maîtriser. Si elle réussit à leur échapper, elle pourrait sauver ses amis, sa famille, et le monde qu’elle a appris à aimer.",122, 0, 'https://www.youtube.com/watch?v=75QXYNz5Q8o');
 
 
 -- --------------------------------------------------------
@@ -106,6 +137,13 @@ ALTER TABLE `genre`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `type`
+--
+ALTER TABLE `type`
+  ADD PRIMARY KEY (`id`);
+
+
+--
 -- Indexes for table `media`
 --
 ALTER TABLE `media`
@@ -138,6 +176,12 @@ ALTER TABLE `genre`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT for table `type`
+--
+ALTER TABLE `type`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `media`
 --
 ALTER TABLE `media`
@@ -164,7 +208,8 @@ ALTER TABLE `user`
 -- Constraints for table `media`
 --
 ALTER TABLE `media`
-  ADD CONSTRAINT `media_genre_id_b1257088_fk_genre_id` FOREIGN KEY (`genre_id`) REFERENCES `genre` (`id`);
+  ADD CONSTRAINT `media_genre_id_b1257088_fk_genre_id` FOREIGN KEY (`genre_id`) REFERENCES `genre` (`id`),
+  ADD CONSTRAINT `media_type_id_b1257088_fk_type_id` FOREIGN KEY (`type_id`) REFERENCES `type` (`id`);
 
 --
 -- Constraints for table `history`
